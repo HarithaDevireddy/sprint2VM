85232b866247d546c159a7bbd768c81345f0b7b0
Devireddy <haritha.devireddy@capgemini.com>
2021-02-16 14:18:40 +0530

    em

--- src/main/java/com/cg/vmtoolapi/service/CustomerService.java
+++ src/main/java/com/cg/vmtoolapi/service/CustomerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.vmtoolapi.domain.Customer;
+import com.cg.vmtoolapi.domain.User;
 import com.cg.vmtoolapi.repository.CustomerRepository;
 
 @Service
@@ -13,6 +14,15 @@ public class CustomerService {
 	private CustomerRepository customerRepository;
 	
 	public Customer saveorUpdateCustomer(Customer customer) {
+		
+		if(customer.getCustomerId()==null) {
+			User user=new User();
+		    customer.setUser(user);
+		    user.setCustomer(customer);
+		    user.setEmailId(customer.getEmailId());
+		    user.setPassword(customer.getPassword());
+			
+		}
 		return customerRepository.save(customer);
 	}
 
